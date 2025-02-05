89890a3fe5206624c5b2b23a35716d8e39924a2e
Inès <charines@outlook.fr>
2025-02-05 11:04:46 +0100

    fix current user in main

--- lib/pages/games/questionnaire/ranking.dart
+++ lib/pages/games/questionnaire/ranking.dart
@@ -1,12 +1,10 @@
-import 'dart:ffi';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';  // Import Firebase Auth
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 class RankingPage extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _Classement();
@@ -14,8 +12,7 @@ class RankingPage extends StatefulWidget {
 }
 
 class _Classement extends State<RankingPage> {
-  
-  
+
   void onButtonPressed() {
     Navigator.push(
       context,
@@ -25,24 +22,32 @@ class _Classement extends State<RankingPage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      
       floatingActionButton: FloatingActionButton(
         onPressed: onButtonPressed,
         tooltip: 'Classement',
         child: const Icon(Icons.emoji_events),
-      ), 
+      ),
     );
   }
 }
+
 class ClassementPage extends StatelessWidget {
   final Stream<QuerySnapshot> _rankingStream = FirebaseFirestore.instance.collection('Users').orderBy("totalpoints", descending: true ).snapshots();
+
+  // Récupérer l'utilisateur connecté avec Firebase Auth
+  Future<String?> getCurrentUserPseudo() async {
+    User? user = FirebaseAuth.instance.currentUser;  // Récupérer l'utilisateur
+    if (user != null) {
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('Users').doc(user.uid).get();
+      return userDoc['pseudo'];  // Récupérer le pseudo de l'utilisateur
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     // backgroundColor: const Color(0xFF0B162C),
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(0, 113, 152, 1),
         foregroundColor: Colors.white,
@@ -58,71 +63,69 @@ class ClassementPage extends StatelessWidget {
           ),
         ),
         child: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: Text(
-                "LES MEILLEURS CODEURS",
-                style:  GoogleFonts.montserrat(textStyle:TextStyle(
-                  color: Colors.white,
-                  fontSize: MediaQuery.of(context).size.width / 16,
-                  fontWeight: FontWeight.bold,
-                ),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: Text(
+                  "LES MEILLEURS CODEURS",
+                  style: GoogleFonts.montserrat(textStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: MediaQuery.of(context).size.width / 16,
+                    fontWeight: FontWeight.bold,
+                  )),
                 ),
               ),
-            ),
-            StreamBuilder<QuerySnapshot>(
-        stream: _rankingStream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Something went wrong: ${snapshot.error}'));
-          }
+              StreamBuilder<QuerySnapshot>(
+                stream: _rankingStream,
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Something went wrong: ${snapshot.error}'));
+                  }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          int rank = 1; 
-          for (int i=1 ;  i<=20 ; i++){
-            rank+=1; 
-          }
-          return Column(
-            children: snapshot.data!.docs.asMap().entries.map((entry) {
-            int rank = entry.key + 1; // Numérotation des rangs
-            DocumentSnapshot document = entry.value;
-            Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-            return classementCard(rank: rank, name: data['pseudo'], url: "debug-master", points: data['totalpoints'].toString());
-            }).toList(),
-          );
-        },
-      ), /*
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  classementCard(rank: 1, name: "Maître du Debug", url: "debug-master", points: "2500"),
-                  classementCard(rank: 2, name: "Hacker Nocturne", url: "night-hacker", points: "2300"),
-                  classementCard(rank: 3, name: "Bug Slayer", url: "bug-slayer", points: "2200"),
-                  classementCard(rank: 4, name: "Compilateur Humain", url: "human-compiler", points: "2100"),
-                  classementCard(rank: 5, name: "Code Alchimiste", url: "code-alchemist", points: "2000"),
-                  classementCard(rank: 6, name: "Architecte du Cloud", url: "cloud-architect", points: "1950"),
-                  classementCard(rank: 7, name: "Guru des Algorithmes", url: "algo-guru", points: "1900"),
-                  classementCard(rank: 8, name: "Samouraï du Terminal", url: "terminal-samurai", points: "1850"),
-                ],
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+
+                  return FutureBuilder<String?>(
+                    future: getCurrentUserPseudo(),  // Récupérer le pseudo de l'utilisateur connecté
+                    builder: (BuildContext context, AsyncSnapshot<String?> userPseudoSnapshot) {
+                      if (userPseudoSnapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      String? currentUserPseudo = userPseudoSnapshot.data;
+
+                      return Column(
+                        children: snapshot.data!.docs.asMap().entries.map((entry) {
+                          int rank = entry.key + 1; // Numérotation des rangs
+                          DocumentSnapshot document = entry.value;
+                          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                          String playerPseudo = data['pseudo'];
+                          Color textColor = playerPseudo == currentUserPseudo ? const Color.fromARGB(255, 141, 227, 255) : Colors.white; // Si c'est l'utilisateur, couleur verte
+
+                          return classementCard(
+                            rank: rank,
+                            name: playerPseudo,
+                            url: "debug-master",
+                            points: data['totalpoints'].toString(),
+                            textColor: textColor,  // Appliquer la couleur du texte
+                          );
+                        }).toList(),
+                      );
+                    },
+                  );
+                },
               ),
-            ),*/
-          ],
+            ],
+          ),
         ),
-      ),
-
       ),
     );
   }
 }
 
-Widget classementCard({required int rank, required String name, required String url, required String points}) {
-  
-
+Widget classementCard({required int rank, required String name, required String url, required String points, required Color textColor}) {
   return Container(
     margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
     padding: const EdgeInsets.all(12.0),
@@ -139,8 +142,7 @@ Widget classementCard({required int rank, required String name, required String 
             fontSize: 18,
             fontWeight: FontWeight.bold,
             color: rank <= 3 ? Colors.amber : Colors.white,
-          ),
-        ),
+          )),
         ),
         const SizedBox(width: 12),
         CircleAvatar(
@@ -155,7 +157,7 @@ Widget classementCard({required int rank, required String name, required String 
               Text(
                 name,
                 style: TextStyle(
-                  color: name == "Hacker Nocturne" ? Color.fromARGB(255, 148, 199, 229) : Colors.white,
+                  color: textColor,  // Couleur du texte en fonction de l'utilisateur connecté
                   fontWeight: FontWeight.bold,
                   fontSize: 18,
                 ),
