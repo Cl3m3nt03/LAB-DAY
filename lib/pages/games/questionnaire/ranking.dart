fe992415127b23f51ec65c302c21e231bd4201b8
Inès <charines@outlook.fr>
2025-02-05 12:32:04 +0100

    add avatar for each users

--- lib/pages/games/questionnaire/ranking.dart
+++ lib/pages/games/questionnaire/ranking.dart
@@ -1,10 +1,16 @@
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';  // Import Firebase Auth
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:makeitcode/widget/auth.dart';
+import 'package:makeitcode/widget/system_getAvatar.dart';
 
 class RankingPage extends StatefulWidget {
+  const RankingPage({super.key});
+  
   @override
   State<StatefulWidget> createState() {
     return _Classement();
@@ -12,7 +18,6 @@ class RankingPage extends StatefulWidget {
 }
 
 class _Classement extends State<RankingPage> {
-
   void onButtonPressed() {
     Navigator.push(
       context,
@@ -33,16 +38,24 @@ class _Classement extends State<RankingPage> {
 }
 
 class ClassementPage extends StatelessWidget {
-  final Stream<QuerySnapshot> _rankingStream = FirebaseFirestore.instance.collection('Users').orderBy("totalpoints", descending: true ).snapshots();
+  final Stream<QuerySnapshot> _rankingStream = FirebaseFirestore.instance
+      .collection('Users')
+      .orderBy("totalpoints", descending: true)
+      .snapshots();
 
-  // Récupérer l'utilisateur connecté avec Firebase Auth
   Future<String?> getCurrentUserPseudo() async {
-    User? user = FirebaseAuth.instance.currentUser;  // Récupérer l'utilisateur
+    User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('Users').doc(user.uid).get();
-      return userDoc['pseudo'];  // Récupérer le pseudo de l'utilisateur
+      DocumentSnapshot userDoc =
+          await FirebaseFirestore.instance.collection('Users').doc(user.uid).get();
+      return userDoc['pseudo'];
     }
     return null;
+  }
+
+  Future<Uint8List?> getAvatarForUser(String uid) async {
+    // Charger l'avatar de l'utilisateur ici
+    return AvatarService.getUserAvatar(uid);
   }
 
   @override
@@ -52,7 +65,7 @@ class ClassementPage extends StatelessWidget {
         backgroundColor: const Color.fromRGBO(0, 113, 152, 1),
         foregroundColor: Colors.white,
       ),
-      body:Container(
+      body: Container(
         height: MediaQuery.of(context).size.height,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
@@ -69,11 +82,13 @@ class ClassementPage extends StatelessWidget {
                 margin: const EdgeInsets.all(10),
                 child: Text(
                   "LES MEILLEURS CODEURS",
-                  style: GoogleFonts.montserrat(textStyle: TextStyle(
-                    color: Colors.white,
-                    fontSize: MediaQuery.of(context).size.width / 16,
-                    fontWeight: FontWeight.bold,
-                  )),
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      color: Colors.white,
+                      fontSize: MediaQuery.of(context).size.width / 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ),
               StreamBuilder<QuerySnapshot>(
@@ -88,7 +103,7 @@ class ClassementPage extends StatelessWidget {
                   }
 
                   return FutureBuilder<String?>(
-                    future: getCurrentUserPseudo(),  // Récupérer le pseudo de l'utilisateur connecté
+                    future: getCurrentUserPseudo(),
                     builder: (BuildContext context, AsyncSnapshot<String?> userPseudoSnapshot) {
                       if (userPseudoSnapshot.connectionState == ConnectionState.waiting) {
                         return const Center(child: CircularProgressIndicator());
@@ -98,18 +113,34 @@ class ClassementPage extends StatelessWidget {
 
                       return Column(
                         children: snapshot.data!.docs.asMap().entries.map((entry) {
-                          int rank = entry.key + 1; // Numérotation des rangs
+                          int rank = entry.key + 1;
                           DocumentSnapshot document = entry.value;
                           Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
                           String playerPseudo = data['pseudo'];
-                          Color textColor = playerPseudo == currentUserPseudo ? const Color.fromARGB(255, 141, 227, 255) : Colors.white; // Si c'est l'utilisateur, couleur verte
+                          String playerUid = document.id; // Assuming UID is stored as the document ID
+                          Color textColor = playerPseudo == currentUserPseudo
+                              ? const Color.fromARGB(255, 141, 227, 255)
+                              : Colors.white;
 
-                          return classementCard(
-                            rank: rank,
-                            name: playerPseudo,
-                            url: "debug-master",
-                            points: data['totalpoints'].toString(),
-                            textColor: textColor,  // Appliquer la couleur du texte
+                          // Utiliser un FutureBuilder pour charger l'avatar de manière indépendante
+                          return FutureBuilder<Uint8List?>(
+                            future: getAvatarForUser(playerUid),
+                            builder: (BuildContext context, AsyncSnapshot<Uint8List?> avatarSnapshot) {
+                              if (avatarSnapshot.connectionState == ConnectionState.waiting) {
+                                return const Center(child: CircularProgressIndicator());
+                              }
+
+                              Uint8List? avatarImage = avatarSnapshot.data;
+
+                              return classementCard(
+                                rank: rank,
+                                name: playerPseudo,
+                                url: "debug-master", // Replace with the correct avatar URL
+                                points: data['totalpoints'].toString(),
+                                textColor: textColor,
+                                avatarImage: avatarImage,  // Pass avatar image to the card
+                              );
+                            },
                           );
                         }).toList(),
                       );
@@ -125,7 +156,14 @@ class ClassementPage extends StatelessWidget {
   }
 }
 
-Widget classementCard({required int rank, required String name, required String url, required String points, required Color textColor}) {
+Widget classementCard({
+  required int rank,
+  required String name,
+  required String url,
+  required String points,
+  required Color textColor,
+  required Uint8List? avatarImage,
+}) {
   return Container(
     margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
     padding: const EdgeInsets.all(12.0),
@@ -138,7 +176,7 @@ Widget classementCard({required int rank, required String name, required String 
       children: [
         Text(
           "$rank.",
-          style: GoogleFonts.montserrat(textStyle:TextStyle(
+          style: GoogleFonts.montserrat(textStyle: TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.bold,
             color: rank <= 3 ? Colors.amber : Colors.white,
@@ -147,7 +185,9 @@ Widget classementCard({required int rank, required String name, required String 
         const SizedBox(width: 12),
         CircleAvatar(
           radius: 24,
-          backgroundImage: AssetImage("assets/$url.jpg"),
+          backgroundImage: avatarImage != null
+              ? MemoryImage(avatarImage)
+              : const AssetImage("assets/splashscreen/icon.png") as ImageProvider,
         ),
         const SizedBox(width: 15),
         Expanded(
@@ -157,7 +197,7 @@ Widget classementCard({required int rank, required String name, required String 
               Text(
                 name,
                 style: TextStyle(
-                  color: textColor,  // Couleur du texte en fonction de l'utilisateur connecté
+                  color: textColor,
                   fontWeight: FontWeight.bold,
                   fontSize: 18,
                 ),
@@ -166,7 +206,7 @@ Widget classementCard({required int rank, required String name, required String 
                 "$points points",
                 style: const TextStyle(
                   color: Colors.white70,
-                  fontStyle: FontStyle.italic, 
+                  fontStyle: FontStyle.italic,
                   fontSize: 16,
                 ),
               ),
