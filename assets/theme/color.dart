14bb21232c3d37c2936bfb34ca6d303b5de6aeda
Mateis B <mateisbourlet9@gmail.com>
2025-03-18 16:11:54 +0100

    merge detailmodif

---
+++ assets/theme/color.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+
+
+// COMMUN 
+const Color dark = Colors.black; 
+const Color white = Colors.white; 
+const Color softWhite = Colors.white70;
+const Color midnightBlue = Color.fromARGB(255, 11, 22, 44);
+const Color skyBlue = Color.fromRGBO(0, 113, 152, 1);
+const Color pureWhite = Color.fromARGB(255, 255, 255, 255);
+const Color pureBlack = Color.fromARGB(255, 0, 0, 0);
+const Color amber = Colors.amber;
+const Color oceanBlue = Color.fromARGB(255, 6, 146, 194);
+const Color deepAqua = Color.fromRGBO(6, 146, 194, 1);
+const Color deepNavy = Color.fromARGB(255, 11, 22, 44);
+const Color vibrantBlue = Color.fromRGBO(11, 153, 253, 1);
+const Color sandyBrown = Color.fromARGB(250, 175, 142, 88);
+const Color semiTransparentBlack = Colors.black26;
+
+
+// Autre page 
+const Color blue = Colors.blue;
+const Color red = Colors.red ;
+const Color deepSlateBlue = Color.fromARGB(255, 50, 52, 76);
+const Color darkPeriwinkle = Color.fromARGB(255, 222, 226, 255);
+const Color mistySlate = Color.fromRGBO(119, 146, 155, 0.7);
+const Color slateGrey = Color.fromARGB(255, 79, 89, 115);
+const Color semiTransparentWhite = Colors.white24;
+Color darkGrey = Colors.grey[800]!;
+Color darkGreyShade = Colors.grey.shade700;
+Color lightBlue = Colors.blue[100]!;
+const Color softLavender = Color.fromARGB(104, 191, 199, 237);
+const Color lightLavender = Color.fromARGB(255, 191, 199, 237);
+const Color paleLavender = Color.fromARGB(255, 232, 228, 243);
+const Color lightSteelBlue = Color.fromARGB(255, 169, 186, 220);
+const Color darkPurple = Color.fromARGB(255, 47, 3, 69);
+const Color deepShadow = Color.fromARGB(255, 59, 49, 64);
+
+
+
+// Global_chat 
+Color userMessageBlue = Colors.blue.shade300; 
+Color otherMessageGray = Colors.grey.shade300; 
+
+
+// Projet_detail_page
+const Color seafoamGreen = Color(0xff5FC2BA);
+const Color goldenrodYellow = Color(0xffE8B228);
+const Color midnightBlueDark = Color.fromARGB(255, 11, 15, 44);
+const Color deepBlue = Color.fromRGBO(52, 96, 148, 1);
+const Color steelBlue = Color.fromARGB(255, 52, 96, 148);
+const Color softGreen = Color.fromARGB(255, 160, 202, 133);
+const Color fieryRed = Color.fromARGB(255, 175, 58, 54);
+const Color mutedAmethyst = Color.fromRGBO(141, 117, 179, 0.702);
+const Color twilightPurple = Color.fromRGBO(70, 46, 109, 0.851);
+const Color lightIceBlue = Color.fromARGB(255, 225, 240, 254);
+const Color yellow = Colors.yellow; 
+const Color grey = Colors.grey; 
+const Color midnightIndigo = Color.fromARGB(255, 18, 27, 56);
+
+// Questionnaire_list_page
+const Color darkSlateBlue = Color.fromARGB(204, 24, 37, 63);
+
+
+// Ranking 
+const Color mutedPurple = Color.fromARGB(255, 94, 79, 115);
+const Color lightSkyBlue = Color.fromARGB(255, 209, 223, 255);
+const Color paleCyan =  Color.fromARGB(255, 141, 227, 255);
+Color smokyPurple =  Color(0xFF5E4F73).withOpacity(0.85);
+
+// Login
+const Color burntCaramel = Color.fromARGB(249, 161, 119, 51);
+const Color goldenOak = Color.fromARGB(248, 211, 157, 70);
+
+// Open_profil
+const Color shadowBlack = Color.fromRGBO(0, 0, 0, 0.3);
+
+// Profile_page
+const Color twilightShadow = Color.fromRGBO(24, 37, 63, 0.4);
+const Color translucentWhite = Color.fromARGB(70, 255, 255, 255);
+
+// Home_page
+const Color lightGrey = Color(0xffE6E6E6);  
+const Color mediumGrey = Color(0xffC6C6C6); 
+
+// NavBar
+const Color darkNight = Color.fromARGB(255, 19, 19, 25);
+const Color deepOcean = Color.fromRGBO(11, 22, 44, 1);
+
+// ProgressBar
+const Color blackShadow = Color(0xff010101);
+const Color goldenGlow = Color(0xffE8B228);
+
+// Project_card
+const Color darkSlate = Color(0xff0b0c0d);
+const Color deepBlueShade = Color(0xff0d1e30);
+
+// rewardScreen 
+const Color offWhite = Color(0xfffdfffd);
+const Color beigeTan = Color(0xffd4b394);
+const Color lightGray = Color(0xffcad0cf);
+const Color warmGray = Color(0xff9c9790);
